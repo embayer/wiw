1856e0d48973d1cdea58af216be6d9ed9c385604
Markus Bayer <bayer_markus@web.de>
2015-02-16 21:39:56 +0100

    better algorithm for cumulative distribution in letter selection

--- wiw/Lexi.swift
+++ wiw/Lexi.swift
@@ -93,6 +93,40 @@ let alphabet: [Character] = [
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "ä", "ö", "ß", "ü"
 ]
 
+let letterFrequency: [Character: [String: Double]] = [
+    "e": ["from": 83.08, "to": 100.00],     // 16.92
+    "n": ["from": 72.55, "to": 83.08],      // 10.53
+    "i": ["from": 64.53, "to": 72.55],      // 8.02
+    "r": ["from": 57.64, "to": 64.53],      // 6.89
+    "s": ["from": 51.22, "to": 57.64],      // 6.42
+    "t": ["from": 45.43, "to": 51.22],      // 5.79
+    "a": ["from": 39.85, "to": 45.43],      // 5.58
+    "h": ["from": 34.87, "to": 39.85],      // 4.98
+    "d": ["from": 29.89, "to": 34.87],      // 4.98
+    "u": ["from": 26.06, "to": 29.89],      // 3.83
+    "l": ["from": 22.46, "to": 26.06],      // 3.60
+    "c": ["from": 19.30, "to": 22.46],      // 3.16
+    "g": ["from": 16.28, "to": 19.30],      // 3.02
+    "m": ["from": 13.73, "to": 16.28],      // 2.55
+    "o": ["from": 11.49, "to": 13.73],      // 2.24
+    "b": ["from": 9.53, "to": 11.49],       // 1.96
+    "w": ["from": 7.75, "to": 9.53],        // 1.78
+    "f": ["from": 6.26, "to": 7.75],        // 1.49
+    "k": ["from": 4.94, "to": 6.26],        // 1.32
+    "z": ["from": 3.73, "to": 4.94],        // 1.21
+    "v": ["from": 2.89, "to": 3.73],        // 0.84
+    "p": ["from": 2.22, "to": 2.89],        // 0.67
+    "ü": ["from": 1.57, "to": 2.22],        // 0.65
+    "ä": ["from": 1.03, "to": 1.57],        // 0.54
+    "ß": ["from": 0.66, "to": 1.03],        // 0.37
+    "ö": ["from": 0.36, "to": 0.66],        // 0.30
+    "j": ["from": 0.12, "to": 0.36],        // 0.24
+    "y": ["from": 0.07, "to": 0.12],        // 0.05
+    "x": ["from": 0.02, "to": 0.07],        // 0.05
+    "q": ["from": 0.00, "to": 0.02],        // 0.02
+]
+
+
 // TODO sub ß from s...
 let letterFrequency: [Character: Double] = [
     "e": 17.40,
@@ -194,6 +228,19 @@ func randomRangeFloat(lower: Double, upper: Double) -> Float {
     
     return Float((Double(arc4random()) / arc4randoMax) * (upper - lower) + lower)
 }
+
+func getRandomLetter2121(difficulty: Int) -> Character {
+    // dice the percentage
+    var freq: Float = randomRangeFloat(0.00, 100.00)
+    
+    // find the letter in that range
+    for (letter, fromTo) in letterFrequency {
+        if((freq > fromTo["from"]) && freq <= fromTo["to"]) {
+            return letter
+        }
+    }
+}
+
 
 func getRandomLetter(difficulty: Int) -> Character {
     var letter: Character? = nil
